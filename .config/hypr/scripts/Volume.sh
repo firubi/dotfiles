c357a95e584943fa17134d5fc693a70e1c901e8a
firubi <168989779+firubi@users.noreply.github.com>
2024-06-30 18:01:28 +0200

    Add files via upload

---
+++ .config/hypr/scripts/Volume.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+iDIR="$HOME/.config/dunst/Icons"
+
+# Get Volume
+get_volume() {
+    volume=$(pamixer --get-volume)
+    if [[ "$volume" -eq "0" ]]; then
+        echo "Muted"
+    else
+        echo "$volume%"
+    fi
+}
+
+# Get icons
+get_icon() {
+    current=$(get_volume)
+    if [[ "$current" == "Muted" ]]; then
+        echo "$iDIR/mute"
+    elif [[ "${current%\%}" -le 30 ]]; then
+        echo "$iDIR/low"
+    elif [[ "${current%\%}" -le 60 ]]; then
+        echo "$iDIR/normal"
+    else
+        echo "$iDIR/urgent"
+    fi
+}
+
+# Notify
+notify_user() {
+    if [[ "$(get_volume)" == "Muted" ]]; then
+        notify-send -e -h string:x-canonical-private-synchronous:volume_notif -u low -i "$(get_icon)" "Volume: Muted"
+    else
+        notify-send -e -h int:value:"$(get_volume | sed 's/%//')" -h string:x-canonical-private-synchronous:volume_notif -u low -i "$(get_icon)" "現在: $(get_volume)"
+    fi
+}
+
+# Increase Volume
+inc_volume() {
+    if [ "$(pamixer --get-mute)" == "true" ]; then
+        toggle_mute
+    else
+        pamixer -i 2 --allow-boost --set-limit 150 && notify_user
+    fi
+}
+
+# Decrease Volume
+dec_volume() {
+    if [ "$(pamixer --get-mute)" == "true" ]; then
+        toggle_mute
+    else
+        pamixer -d 2 && notify_user
+    fi
+}
+
+# Toggle Mute
+toggle_mute() {
+	if [ "$(pamixer --get-mute)" == "false" ]; then
+		pamixer -m && notify-send -e -u low -i "$iDIR/mute" "ミュート"
+	elif [ "$(pamixer --get-mute)" == "true" ]; then
+		pamixer -u && notify-send -e -u low -i "$(get_icon)" "現在: $(pamixer --get-volume)%"
+	fi
+}
+
+
+# Execute accordingly
+if [[ "$1" == "--get" ]]; then
+	get_volume
+elif [[ "$1" == "--inc" ]]; then
+	inc_volume
+elif [[ "$1" == "--dec" ]]; then
+	dec_volume
+elif [[ "$1" == "--toggle" ]]; then
+	toggle_mute
+else
+	get_volume
+fi
